7f2a097332394c4ceac06c0cd6b439d70a7ab2cc
Thomas B <tbouquet@gmail.com>
2022-06-25 10:03:27 +0200

    Coorections following massive site update

--- scrape.py
+++ scrape.py
@@ -8,9 +8,12 @@ import plotly.express as px
 import urllib3
 from bs4 import BeautifulSoup
 from selenium import webdriver
+from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 from webdriver_manager.chrome import ChromeDriverManager
 
 from get_otp_code import get_otp_code
@@ -26,14 +29,14 @@ with open('config.json') as fh:
     config = json.loads(fh.read())
 
 
-url_resmed = 'https://myair.resmed.eu/Default.aspx?redirectCountry='+country_code
+url_resmed = 'https://myair2.resmed.com/'
 
 
 def get_page_soup():
 
     # Select "headless"
     options = webdriver.ChromeOptions()
-    options.add_argument("--headless")
+    # options.add_argument("--headless")
 
     # Instantiate the ChromeDriver
     s = Service(ChromeDriverManager().install())
@@ -42,12 +45,27 @@ def get_page_soup():
     # Connect to Resmed website, type in fields and submit
     print('Connecting to Resmed website : ', url_resmed)
     driver.get(url_resmed)
-    driver.find_element(By.NAME,
-                        'ctl00$ctl00$PageContent$MainPageContent$textBoxEmailAddress').send_keys(config['rs_email'])
-    driver.find_element(By.NAME,
-                        'ctl00$ctl00$PageContent$MainPageContent$textBoxPassword').send_keys(config['rs_password'])
-    driver.find_element(By.NAME,
-                        'ctl00$ctl00$PageContent$MainPageContent$textBoxPassword').send_keys(Keys.RETURN)
+
+    # driver.execute_script("__doPostBack('ctl00$ctl00$PageContent$MainPageContent$franceSelector$linkCountry','')")
+    # WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.ID, "ctl00$ctl00$PageContent$MainPageContent$franceSelector$linkCountry"))).click()
+    
+    try:
+        WebDriverWait(driver, 20).until(EC.element_to_be_clickable(
+            (By.XPATH, '//*[@id = "region-EU"]/div/div[1]/ul/li[6]/a'))).click()
+        print("Country chosen")
+    except TimeoutException:
+        print("Country not found")
+    
+    # https://stackoverflow.com/questions/59374469/how-to-paginate-through-the-page-numbers-when-href-contains-javascript-dopostb/59377905#59377905
+
+    # driver.find_element(
+    #     By.ID, 'ctl00$ctl00$PageContent$MainPageContent$franceSelector$linkCountry').click()
+    driver.find_element(By.ID,
+                        'okta-signin-username').send_keys(config['rs_email'])
+    driver.find_element(By.ID,
+                        'okta-signin-password').send_keys(config['rs_password'])
+    driver.find_element(By.ID,
+                        'okta-signin-submit').send_keys(Keys.RETURN)
 
     # Sleep 5 second to receive the OTP email on gmail
     time.sleep(5)
@@ -55,11 +73,11 @@ def get_page_soup():
 
     # GEt the the OTP password and submit
 
-    driver.find_element(By.NAME,
-                        'ctl00$ctl00$PageContent$MainPageContent$textBoxOneTimePassword').send_keys(get_otp_code(config['gm_email'], config['gm_password']))
-    print('Sending OTP password')
     driver.find_element(By.ID,
-                        'ctl00_ctl00_PageContent_MainPageContent_buttonCheckOtp').click()
+                        'input112').send_keys(get_otp_code(config['gm_email'], config['gm_password']))
+    print('Sending OTP password')
+    driver.find_element(By.XPATH,
+                        '//*[@id="form96"]/div[2]/input').send_keys(Keys.RETURN)
 
     # Sleep to have time to switch page
     time.sleep(5)
@@ -97,4 +115,4 @@ if __name__ == '__main__':
         DF = DF.append(pd.DataFrame(month_score))
     DF[['DayNumber', 'Score', 'UsageScore', 'MaskScore', 'EventsScore']] = DF[[
         'DayNumber', 'Score', 'UsageScore', 'MaskScore', 'EventsScore']].astype(int)
-    print(DF.to_csv)
+    print(DF.to_csv())
